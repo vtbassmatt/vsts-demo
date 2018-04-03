d86d414ce98e09151ab04afba219eff51249fbd9
Matt Cooper <vtbassmatt@gmail.com>
2018-04-03 11:01:54 -0400

    Update vsts-logging.py

--- vsts-logging.py
+++ vsts-logging.py
@@ -1,1 +1,6 @@
-print("##vso[task.logissue type=warning; code=42;]Hello world!")
+import uuid
+
+print("##vso[task.logissue type=warning; code=42;]Red team is here")
+
+timeline_id = uuid.uuid4()
+print(f"##vso[task.logdetail id={str(timeline_id)};name=Red Team;type=build;order=1]Hi from Red")
