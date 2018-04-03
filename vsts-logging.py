e1c305c4a143e32920a335b81448707a71516321
Matt Cooper <vtbassmatt@gmail.com>
2018-04-03 11:07:48 -0400

    Update vsts-logging.py

--- vsts-logging.py
+++ vsts-logging.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 
 print("##vso[task.logissue type=warning; code=42;]Red team is here")
@@ -5,4 +6,5 @@ print("##vso[task.logissue type=warning; code=42;]Red team is here")
 timeline_id = uuid.uuid4()
 print(f"##vso[task.logdetail id={str(timeline_id)};name=Red Team;type=build;order=1]Hi from Red")
 
-print("##vso[task.addattachment type=myattachmenttype;name=myattachmentname;]vsts-logging.py")
+repopath = os.environ['BUILD_REPOSITORY_LOCALPATH']
+print(f"##vso[task.addattachment type=myattachmenttype;name=myattachmentname;]{repopath}/vsts-logging.py")
