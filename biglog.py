16d31dad89ac2ac14c46f973bd21796ced29cf94
Matt Cooper <vtbassmatt@gmail.com>
2018-04-04 11:35:20 -0400

    echo back our params

--- biglog.py
+++ biglog.py
@@ -50,6 +50,9 @@ art from http://www.chris.com/ascii/index.php?art=logos%20and%20insignias/jolly%
 min_line_length = int(os.environ.get("MIN_LINE_LENGTH", 1))
 min_line_count = int(os.environ.get("MIN_LINE_COUNT", 1))
 
+print(f"min_line_length = {min_line_length}")
+print(f"min_line_count =  {min_line_count}")
+
 line = jolly_roger * max(1, math.ceil(min_line_length / len(jolly_roger)))
 for x in range(max(1, min_line_count)):
   print(f"##vso[task.logissue type=error;]{line}")
